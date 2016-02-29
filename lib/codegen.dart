ab0447841afefdc079270ed59f43edaf0eb5089c
Don <don.j.olmstead@gmail.com>
2016-02-28 22:44:53 -0800

    Export the codegen libraries

---
+++ lib/codegen.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/codegen/model_class_view.dart';
+export 'src/codegen/serialize_annotation.dart';
