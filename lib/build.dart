869769ee0c6b8ff28a9a5b29829e49110be932dc
Don <don.j.olmstead@gmail.com>
2016-03-03 23:43:57 -0800

    Add build library

---
+++ lib/build.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/build/model_codegen_step.dart';
+export 'src/build/model_view_step.dart';
