652ac9dd751c2e13b62a5196f2d087f6aab2713d
Don <don.j.olmstead@gmail.com>
2016-03-06 09:15:39 -0800

    Update dependencies

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -ex
 
+# Clone build
+git clone https://github.com/dart-lang/build.git ../build
+
 # Clone dogma libraries
 git clone https://github.com/dogma-dart/dogma-convert.git ../dogma-convert
 git clone https://github.com/dogma-dart/dogma-source-analyzer.git ../dogma-source-analyzer
